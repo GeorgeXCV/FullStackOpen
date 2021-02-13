c38433e50cc49ed6902bc0ea99a40e8a6d3e3ffb
GeorgeXCV <georgeashton95@gmail.com>
2021-02-13 09:35:26 +0000

    Finish Part 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -8,77 +8,86 @@ const Phonebook = require('./models/phonebook')
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
-morgan.token('body', (req, res) => {
-    if (req.method === "POST") {
-        return JSON.stringify(req.body);
-    }
-}); 
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+morgan.token('body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/persons', (request, response) => {
-  Phonebook.find({}).then(contacts => {
+app.get('/api/persons', async (request, response, next) => {
+  try {
+    const contacts = await Phonebook.find({})
     response.json(contacts)
-  })
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
+app.get('/api/persons/:id', async (request, response, next) => {
+  try {
     const id = request.params.id
-    Phonebook.findById(id).then(contact => {
-      if (contact) {        
-          response.json(contact)      
-        } else {        
-          response.status(404).end()      
-        }   
-      })
-      .catch(error => next(error))
-  })
-
-app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    
-    Phonebook.findByIdAndRemove(id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/persons', (request, response, next) => {
- 
-    const body = request.body
-    if (!body.name) {
-        return response.status(400).json({ 
-          error: 'Name is required in request body.' 
-        })
-      }
-
-      if (!body.number) {
-        return response.status(400).json({ 
-          error: 'Number is required in request body.' 
-        })
-      }
-    
-    const duplicate = persons.find(person => person.name == body.name)
-    if (duplicate) {
-        request.method = 'PUT'
-        request.url = `/api/persons/${person.id}`
-        next();
-      } else {
-        const person = new Phonebook({
-          name: body.name,
-          number: body.number,
-          id: generateId(),
-        })
-      
-      person.save().then(savedContact => {
-        response.json(savedContact)
-      })
-      .catch(error => next(error))
+    const personResult = await Phonebook.findById(id)
+    if (personResult) {
+      response.json(personResult)
+    } else {
+      response.status(404).end()
     }
+  } catch (error) {
+    next(error)
+  }
 })
 
-app.put('/api/persons/:id', (request, response, next) => {
-  
+app.delete('/api/persons/:id', async (request, response, next) => {
+  try {
+    const id = request.params.id
+    const deletePerson = await Phonebook.findByIdAndRemove(id)
+    if (deletePerson) {
+      response.status(204).end()
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.post('/api/persons', async (request, response, next) => {
+  const body = request.body
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'Name is required in request body.'
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'Number is required in request body.'
+    })
+  }
+
+  const duplicate = await Phonebook.findOne({ 'name': body.name })
+  if (duplicate) {
+    return response.status(400).json({
+      error: `${body.name} is already in the phonebook.`
+    })
+  }
+
+  try {
+    const person = new Phonebook({
+      name: body.name,
+      number: body.number,
+      id: generateId(),
+    })
+    const newContact = await person.save()
+    return response.json(newContact)
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.put('/api/persons/:id', async (request, response, next) => {
+  try {
     const body = request.body
 
     const person = {
@@ -86,27 +95,21 @@ app.put('/api/persons/:id', (request, response, next) => {
       number: body.number,
     }
 
-    Phonebook.findByIdAndUpdate(request.params.id, person, {new: true})
-    .then (result => {
-      response.json(result)
-    })
-    .catch(error => next(error))
- })
-
-app.get('/info', async (request, response) => {
-    const phonebookLength = await Phonebook.count({});
-    response.send(`<p>Phonebook has info for ${phonebookLength} people</p> <br>${ new Date()}<p>`)
-  })
-
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    const updatedPerson = await Phonebook.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
+    return response.json(updatedPerson)
+  } catch (error) {
+    next(error)
+  }
 })
 
-const generateId = () => {
-    return Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
+app.get('/info', async (request, response, next) => {
+  try {
+    const phonebookLength = await Phonebook.count({})
+    response.send(`<p>Phonebook has info for ${phonebookLength} people</p> <br>${ new Date()}<p>`)
+  } catch (error) {
+    next(error)
   }
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -117,10 +120,21 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
+
+const generateId = () => {
+  return Math.floor(Math.random() * Math.floor(Math.random() * Date.now()))
+}
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part3/phonebook-frontend/readme.md
@@ -0,0 +1,3 @@
+# Part 3 
+Making the back-end from Part 3 work with the front-end project from part 2
+https://github.com/GeorgeXCV/FullStackOpen/tree/main/part2/phonebook
